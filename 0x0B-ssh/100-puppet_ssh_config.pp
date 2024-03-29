c26cb1d50afc300795b2801d41d7916b367ce5b1
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-03-29 04:05:33 -0700

    all changes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # client configuration with puppet
 
-file_line {'Turn off password auth':
-ensure => 'present',
-path   => '/root/alx-system_engineering-devops/0x0B-ssh/ssh_config',
-line   => '    PasswordAuthentication no',
+file_line { 'Turn off password auth':
+ensure => present,
+path   => '/etc/ssh/sshd_config',
+line   => 'PasswordAuthentication no',
 }
 
-file_line {'identity file':
-ensure => 'present',
-path   => '/root/alx-system_engineering-devops/0x0B-ssh/ssh_config',
-line   => '    IdentityFile ~/.ssh/school',
+file_line { 'identity file':
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => 'IdentityFile ~/.ssh/school',
 }
