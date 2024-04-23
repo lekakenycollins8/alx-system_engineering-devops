b363d543d66331ff77bbf0f64ff377613b41f94f
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-04-23 04:54:49 -0700

    fetched data from an an api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Uses REST API to gather dater for a given employee"""
+
+
+import requests
+from sys import argv
+
+baseUrl = "https://jsonplaceholder.typicode.com/users"
+employeeId = argv[1]
+url = baseUrl + '/' + employeeId
+response = requests.get(url)
+employeeName = response.json().get('name')
+
+todoUrl = url + "/todos"
+response = requests.get(todoUrl)
+tasks = response.json()
+done = 0
+done_tasks = []
+
+for task in tasks:
+    if task.get('completed'):
+        done_tasks.append(task)
+        done += 1
+
+print("Employee {} is done with tasks ({}/{}):".format(
+    employeeName, done, len(tasks)))
+
+for task in done_tasks:
+        print("\t {}".format(task.get('title')))
