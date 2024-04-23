041c87ba87e095658c944c4b736c992def192196
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-04-23 05:12:04 -0700

    export api content to csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Uses REST API to gather dater for a given employee"""
+
+
+import requests
+from sys import argv
+
+baseUrl = "https://jsonplaceholder.typicode.com/users"
+employeeId = argv[1]
+url = baseUrl + '/' + employeeId
+response = requests.get(url)
+username = response.json().get('username')
+
+todoUrl = url + "/todos"
+response = requests.get(todoUrl)
+tasks = response.json()
+
+with open('{}.csv'.format(employeeId), 'w') as file:
+    for task in tasks:
+        file.write('"{}","{}","{}","{}"\n'.format(
+            employeeId, username, task.get('completed'), task.get('title')))
