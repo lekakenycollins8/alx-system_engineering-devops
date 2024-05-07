f3af8b9d00b65847a28a6dc048b6aba46e76282a
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-05-07 05:52:47 -0700

    api querry for subreddit posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""queries Reddit API"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """prints titles of the first ten hot posts of a subreddit"""
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    user_agent = {"User-Agent": "your bot 0.1"}
+    parameters = {"limit": 10}
+
+    response = requests.get(url, headers=user_agent, params=parameters)
+    try:
+        data = response.json()
+        host_posts = data.get('data').get('children')
+
+        for i in hot_posts:
+            print(i.get('data').get('title'))
+    except Exception:
+        print("None")
